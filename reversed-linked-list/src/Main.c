387efc32b89c5b0116f19a99b557298740c1f70f
zidanomar <hzidanomar@gmail.com>
2022-04-23 08:57:59 +0300

    reversed linked list

---
+++ reversed-linked-list/src/Main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+  int data;
+  struct Node *next;
+};
+
+struct Node* Reverse(struct Node* head) {
+  struct Node *current, *prev, *next;
+  current = head;
+  prev = NULL;
+
+  
+  while (current != NULL)
+  {
+    next = current->next;
+    current->next = prev;
+    prev = current;
+    current = next;
+  }
+
+  head = prev;
+
+  return head;
+}
+
+struct Node* RecursionReverse(struct Node* p) {
+  struct Node* head;
+  if(p -> next == NULL) {
+    head = p;
+    return head;
+  }
+
+  head = RecursionReverse(p->next);
+
+  struct Node* q = p -> next;
+  q -> next = p;
+  p -> next = NULL;
+  return head;
+}
+
+struct Node* Insert(struct Node* head, int data){
+   struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+
+
+   temp -> data = data;
+   temp -> next = NULL;
+
+
+  if(head == NULL) head = temp;
+  else {
+    struct Node* temp1 = head;
+    while (temp1->next != NULL) temp1 = temp1->next;
+    temp1->next = temp;
+  }
+
+  return head;
+}
+
+void Print(struct Node* p) {
+  if (p == NULL) {
+   printf("\n");
+    return;
+  }
+
+  printf(" %d", p->data);
+  Print(p->next);
+}
+
+int main(int argc, char const *argv[])
+{
+  struct Node* head = NULL;
+
+  head = Insert(head,1); 
+  head = Insert(head,2); 
+  head = Insert(head,3); 
+  head = Insert(head,4); 
+
+  Print(head);
+
+  head = Reverse(head);
+
+  Print(head);
+
+  head = RecursionReverse(head);
+  Print(head);
+  
+  return 0;
+}
